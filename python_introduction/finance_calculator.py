26189aedfa2661fe4561daad37e61ed594d9879f
isri21 <israelalemu21@gmail.com>
2024-09-11 15:53:58 +0300

    Fix attempt for advanced task 5

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-income = int(input("Enter your monthly income: "))
-expense = int(input("Enter your total monthly expenses: "))
-saving = income - expense
-p_saving = int((saving * 12) + (saving * 12 * 0.05))
-print(f"Your monthly savings are ${saving}.")
-print(f"Projected savings after one year, with interest, is: ${p_saving}.")
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
+projected_savings = int((monthly_savings * 12) + (monthly_savings * 12 * 0.05))
+print(f"Your monthly savings are ${monthly_savings}.")
+print(f"Projected savings after one year, with interest, is: ${projected_savings}.")

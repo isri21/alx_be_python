b8bbbeeeb1c18bb6cfee5bffbf05592b6183bd14
isri21 <israelalemu21@gmail.com>
2024-09-18 20:51:24 +0300

    Modified Task 3

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,6 +1,7 @@
+size = int(input("Enter the size of the pattern: "))
 n = 0
-while n < 4:
-	for i in range (0, 4):
+while n < size:
+	for i in range (0, size):
 		print("*", end="")
 	print(end='\n')
 	n += 1

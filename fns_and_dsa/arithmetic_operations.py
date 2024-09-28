b3b869bf0bf1a65e7fd643fed4bd6eee32ab05a8
isri21 <israelalemu21@gmail.com>
2024-09-28 21:39:18 +0300

    Modified Task 0

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,14 @@
 def perform_operation(num1, num2, operation):
-	match operation:
-		case "add":
-			return num1 + num2
-		case "subtract":
-			return num1 - num2
-		case "multiply":
-			return num1 * num2
-		case "divide":
-			if num2 == 0:
-				return "Unable to divide by zero"
+	if operation == "add":
+		return num1 + num2
+	elif operation == "subtract":
+		return num1 - num2
+	elif operation == "multiply":
+		return num1 * num2
+	elif operation == "divide":
+		if num2 == 0:
+			return "Unable to divide by zero"
+		else:
 			return num1 / num2
-		case _:
-			return "Please enter a correct operation"
+	else:
+		return "Please enter a correct operation"

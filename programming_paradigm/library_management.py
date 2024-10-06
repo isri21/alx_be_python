d1c51a1f3113382ee1de96278c3e14de6ad07031
isri21 <israelalemu21@gmail.com>
2024-10-06 21:43:20 +0300

    Modified Task 3 #3

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -16,23 +16,23 @@ class Book:
 
 class Library:
 	def __init__(self):
-		self.__books = []
+		self._books = []
 		
 	def add_book(self, book):
-		self.__books.append(book)
+		self._books.append(book)
 		
 	def check_out_book(self, title):
-		for book in self.__books:
+		for book in self._books:
 			if book.title == title:
 				book.set_check_value(True)
 			
 	def return_book(self, title):
-		for book in self.__books:
+		for book in self._books:
 			if book.title == title:
 				book.set_check_value(False)
 				
 	def list_available_books(self):
-		for book in self.__books:
+		for book in self._books:
 			if book.is_available() == False:
 				print(f"{book.title} by {book.author}")
 

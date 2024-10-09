09f974ad023dd76450a278b849935f7255e52e75
isri21 <israelalemu21@gmail.com>
2024-10-09 21:22:05 +0300

    Modified Task 1 #0

--- oop/library_system.py
+++ oop/library_system.py
@@ -23,8 +23,8 @@ class PrintBook(Book):
 		return "PrintBook"
 
 class Library:
-	def __init__(self, books=[]):
-		self.books = books
+	def __init__(self):
+		self.books = []
 	
 	def add_book(self, book):
 		self.books.append(book)
